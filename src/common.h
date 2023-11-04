d21b7a15230da8086e266be4e582d02d6f1157f2
erlingrj <erling.jellum@gmail.com>
2023-11-04 15:40:35 +0100

    Update

--- src/common.h
+++ src/common.h
@@ -3,7 +3,7 @@
 
 #include "kiss_fft.h"
 
-#define SAMPLE_SIZE 1024
+#define SAMPLE_SIZE 128
 #define SAMPLE_TYPE float
 #define SAMPLE_RATE 44100
 #define SAMPLE_NUM_BYTES SAMPLE_SIZE*sizeof(SAMPLE_TYPE)
