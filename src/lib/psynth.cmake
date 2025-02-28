634a8bd4b734f0a906fcf0ec7174a5cb52336150
erlingrj <erling.jellum@gmail.com>
2025-02-28 21:02:07 +0100

    WIP: Porting the synth to reactor-uc

--- src/lib/psynth.cmake
+++ src/lib/psynth.cmake
@@ -1,3 +1,10 @@
 target_link_libraries(${LF_MAIN_TARGET} PRIVATE pulse-simple)
 target_link_libraries(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/libkissfft-float.a)
-target_sources(${LF_MAIN_TARGET} PRIVATE freq.c buffer.c midi.c)
+target_include_directories(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR})
+target_include_directories(${LF_MAIN_TARGET} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/../../external/kissfft)
+
+target_sources(${LF_MAIN_TARGET} PRIVATE 
+  ${CMAKE_CURRENT_LIST_DIR}/freq.c
+  ${CMAKE_CURRENT_LIST_DIR}/buffer.c 
+  ${CMAKE_CURRENT_LIST_DIR}/midi.c
+  )
